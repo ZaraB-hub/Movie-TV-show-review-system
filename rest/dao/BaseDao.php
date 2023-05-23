7ee8b95f62c6c5fb91d5a80f019a935ec17aac21
Zara <zarabahtanovic@gmail.com>
2023-05-23 22:17:46 +0200

    db conn

--- rest/dao/BaseDao.php
+++ rest/dao/BaseDao.php
@@ -11,11 +11,13 @@
             {
                 try {
                     $this->table_name = $table_name;
-                    $servername = Config::DB_HOST();
-                    $username = Config::DB_USERNAME();
-                    $password = Config::DB_PASSWORD();
-                    $schema = Config::DB_SCHEMA();;
-                    $this->conn = new PDO("mysql:host=$servername;dbname=$schema", $username, $password);
+                    // $servername = Config::DB_HOST();
+                    // $username = Config::DB_USERNAME();
+                    // $password = Config::DB_PASSWORD();
+                    // $schema = Config::DB_SCHEMA();;
+                    // $this->conn = new PDO("mysql:host=$servername;dbname=$schema", $username, $password);
+                    $this->conn = new PDO("mysql:host=" . Config::DB_HOST() . ";port=" . Config::DB_PORT() . ";dbname=" . Config::DB_SCHEME(), Config::DB_USERNAME(), Config::DB_PASSWORD());
+
                     // set the PDO error mode to exception
                     $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                     //echo "Connected successfully";
